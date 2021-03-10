382de20aa1727dcb572a1b4d6dd1718d120a5c61
lyh1999 <2086435142@qq.com>
2021-03-10 19:17:58 +0800

    页面结构初始化

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <!-- 局部刷新 -->
+  <keep-alive>
+    <router-view></router-view>
+  </keep-alive>
+</template>
+
+
---
+++ src/router/mine/index.js
@@ -0,0 +1,6 @@
+export default {
+    path: '/mine',
+    // 按需导入
+    component: () =>
+        import ('@/views/Mine')
+}
